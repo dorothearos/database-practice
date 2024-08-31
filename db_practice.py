1198b2037c45214bfb794c711efc8dba31d3842e
Doro R <dorothearos@outlook.com>
2024-08-30 23:29:46 -0400

    First commit

---
+++ db_practice.py
@@ -0,0 +1,32 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('STAFF.db')
+
+table_name = "Deparments"
+attribute_list = ["DEPT_ID", "DEP_NAME", "MANAGER_ID", "LOC_ID"]
+
+# https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-PY0221EN-Coursera/labs/v2/Departments.csv
+
+file_path = '/home/project/Departments.csv'
+df = pd.read_csv(file_path, names = attribute_list)
+
+df.to_sql(table_name, conn, if_exists = 'replace', index =False)
+print('Table is ready')
+
+query_statement = f"SELECT * FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+query_statement = f"SELECT DEP_NAME FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+query_statement = f"SELECT COUNT(*) FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+conn.close()
